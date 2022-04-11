f200973c19279ca704c9fefed6cf1db1526b4ac1
Tsubasa <scientistjishan@gmail.com>
2022-04-11 18:31:00 +0530

    Added support for both max and min heap.

--- lib/heap.h
+++ lib/heap.h
@@ -2,8 +2,8 @@
     #define HEAP_H
     #include "./vector.h"
     
-    void heapify(int, int *);
-    int heappush(Vector *, int);
-    int heappop(Vector *);
-    int heappushpop(Vector *, int);
+    void heapify(int, int *, bool);
+    int heappush(Vector *, int, bool);
+    int heappop(Vector *, bool);
+    int heappushpop(Vector *, int, bool);
 #endif
