0ea8f01fafe37560f2931880b70837ef2dae802c
Tsubasa <scientistjishan@gmail.com>
2022-04-11 20:09:36 +0530

    Changed to appropriate type for few functions.

--- lib/heap.h
+++ lib/heap.h
@@ -3,9 +3,9 @@
     #include <stdbool.h>
     #include "./vector.h"
     
-    void heapify(int, int *, bool);
+    void heapify(size_t, int *, bool);
     int heappush(Vector *, int, bool);
     int heappop(Vector *, bool);
     int heappushpop(Vector *, int, bool);
-    void decreasekey(int, int *, int, bool);
+    void decreasekey(size_t, int *, int, bool);
 #endif
