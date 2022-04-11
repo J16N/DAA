70747cec95e90c3008e97bbfea39c1a8ee740828
Tsubasa <scientistjishan@gmail.com>
2022-04-11 19:44:41 +0530

    Added flag parameter in decreasekey operation.

--- lib/heap.h
+++ lib/heap.h
@@ -1,10 +1,11 @@
 #ifndef HEAP_H
     #define HEAP_H
+    #include <stdbool.h>
     #include "./vector.h"
     
     void heapify(int, int *, bool);
     int heappush(Vector *, int, bool);
     int heappop(Vector *, bool);
     int heappushpop(Vector *, int, bool);
-    void decreasekey(int, int *, int);
+    void decreasekey(int, int *, int, bool);
 #endif
