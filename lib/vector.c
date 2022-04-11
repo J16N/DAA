0b05305832f4abefc87929d8effc94cb66eaf699
Tsubasa <scientistjishan@gmail.com>
2022-04-11 12:21:48 +0530

    Implemented vector in C.

---
+++ lib/vector.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "./vector.h"
+
+
+Vector *initV(void)
+{
+    Vector *v = malloc(sizeof(Vector));
+    v->size = 0;
+    v->length = 0;
+    v->arr = NULL;
+    return v;
+}
+
+
+int push(Vector *v, int data)
+{
+    if (v->length == v->size) {
+        v->size = 2 * v->size + 1;
+        v->arr = malloc(v->size * sizeof(int));
+        
+        if (v->arr == NULL)
+            return -1;
+    }
+    
+    v->arr[v->length++] = data;
+    return 0;
+}
+
+
+int pop(Vector *v)
+{
+    int data = v->arr[--(v->length)];
+    
+    if (2 * v->length == v->size - 1) {
+        v->size = v->length;
+        v->arr = malloc(v->size * sizeof(int));
+        
+        if (v->arr == NULL)
+            return -1;
+    }
+    
+    return data;
+}
+
+
+void destroyV(Vector *v)
+{
+    free(v->arr);
+    free(v);
+}
