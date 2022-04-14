744ee0ab6b5d90e693b6e9d833b5467618d1d085
Tsubasa <scientistjishan@gmail.com>
2022-04-14 14:44:59 +0530

    Fixed some problems in `vpush()` and `vpop()` functions.

--- lib/vector.c
+++ lib/vector.c
@@ -1,42 +1,50 @@
 #include <stdlib.h>
+#include <string.h>
 #include "./vector.h"
 
 
-Vector *initV(size_t size)
+Vector *initV(size_t size, size_t elem_sz)
 {
     Vector *v = malloc(sizeof(Vector));
     v->size = size;
     v->length = 0;
-    v->arr = malloc(size * sizeof(int));
+    v->elem_sz = elem_sz;
+    v->arr = malloc(size * elem_sz);
     return v;
 }
 
 
-int vpush(Vector *v, int data)
+int vpush(Vector *v, void *data, size_t data_sz)
 {
+    if (data_sz != v->elem_sz)
+        return -1;
+    
     if (v->length == v->size) {
         v->size = 2 * v->size + 1;
-        v->arr = malloc(v->size * sizeof(int));
+        v->arr = realloc(v->arr, v->size * v->elem_sz);
         
         if (v->arr == NULL)
             return -1;
     }
     
-    v->arr[v->length++] = data;
+    memcpy(v->arr + v->length * v->elem_sz, data, v->elem_sz);
+    v->length++;
     return 0;
 }
 
 
-int vpop(Vector *v)
+void *vpop(Vector *v)
 {
-    int data = v->arr[--(v->length)];
+    if (!v->length)
+        return NULL;
+    
+    v->length--;
+    void *data = malloc(v->elem_sz);
+    memcpy(data, v->arr + v->length * v->elem_sz, v->elem_sz);
     
     if (2 * v->length == v->size - 1) {
         v->size = v->length;
-        v->arr = malloc(v->size * sizeof(int));
-        
-        if (v->arr == NULL)
-            return -1;
+        v->arr = realloc(v->arr, v->size * v->elem_sz);
     }
     
     return data;
