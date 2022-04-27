c4dc8fc4142cec721f33974510d4168e97bb8f2b
Tsubasa <scientistjishan@gmail.com>
2022-04-28 00:15:30 +0530

    Added the header for decrease key operation.

--- lib/heap.h
+++ lib/heap.h
@@ -6,5 +6,6 @@
     int heappush(Vector *, void *, size_t, int (*)(const void *, const void *));
     void *heappop(Vector *, int (*)(const void *, const void *));
     void *heappushpop(Vector *, void *, int (*)(const void *, const void *));
-    void decreasekey(size_t, int *, int, int (*)(const void *, const void *));
+    void decreaseKey(void *, size_t, size_t, void *, 
+                     void (*)(void *, void *), int (*)(const void *, const void *));
 #endif
